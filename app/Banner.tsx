6d69d2f51cb4385957fa12c481a7fe1bdbfb4722
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-10-27 15:20:30 +0000

    Fix 01 Banner

---
+++ app/Banner.tsx
@@ -0,0 +1,15 @@
+export default function Banner() {
+    return (
+
+<div className="md:flex items-center flex-row-reverse bg-[#4d4d4d]">
+  <div className="basis-1/2">
+    <img src="/img/e-commerce-powerhouse-made-by-UTFPR.svg" alt="E commerce" />
+  </div>
+  <div className="md:flex items-center flex-row-reverse bg-[#4d4d4d]"></div>
+  <img className="relative self-stretch max-w-[634.23px] w-[100px]" alt="Image" src="/img/image.jpg" />
+  </div>
+
+    )
+
+}
+  
